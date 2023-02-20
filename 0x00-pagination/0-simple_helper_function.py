03ab8e4ab5439d7be1ed3e349a43b5aa955ea213
Alien <alongejoshua@gmail.com>
2023-02-20 18:50:57 +0100

    returns tuple of range of indexes for particular pagination parameters

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+""" Simple helper function that returns a tuple of size two """
+def index_range(page: int, page_size: int) -> tuple():
+    """ 
+    Index_range - range of indexes concerning pagination parameter
+    args:
+            page
+            page_size
+            
+    Return: tuple of range of indexes"""
+    return (page, page_size)
