87f0a3b81a4abcbc8a809cb919cf250a01cf3db3
Alien <alongejoshua@gmail.com>
2023-02-28 23:44:23 +0100

    startup the app with flask

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python3
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """ the index renders an homepage """
     return render_template('0-index.html')
